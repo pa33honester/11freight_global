4d4df98a5a6362c8fe4d9f165a438c52ea793b74
honester <pa33honester@gmail.com>
2026-01-27 23:11:03 -0600

    upload: remove laravel logo and rebuild

--- public/build/assets/Show-CPaQg7jF.js
+++
@@ -1,1 +0,0 @@
-import{d as e,z as t,x as o,c as n,o as s}from"./app-Ce1PEk3E.js";const _=e({__name:"Show",setup(r){return t(()=>{o.get("/supplier-settlements")}),(a,c)=>(s(),n("div"))}});export{_ as default};
---
+++ public/build/assets/Show-HJ3FKVtE.js
@@ -0,0 +1,1 @@
+import{d as e,z as t,x as o,c as n,o as s}from"./app-C3z3C6Qr.js";const _=e({__name:"Show",setup(r){return t(()=>{o.get("/supplier-settlements")}),(a,c)=>(s(),n("div"))}});export{_ as default};
